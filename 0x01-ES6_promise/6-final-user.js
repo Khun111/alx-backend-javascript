0e3daaaebf59754711c218b635173ad703fafa6d
Oluwatobi <amure387@gmail.com>
2023-06-29 22:43:21 +0000

    fix: task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,5 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(fileName, lastName), uploadPhoto(fileName)];
   return Promise.all(promises)
-    .then((results) => results.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : result.reason })));
+    .then((results) => results.map((result) => ({ status: result.status, value: result.value === undefined ? result.reason : result.value })));
 }
