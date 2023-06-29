8be4e0d0722cf28da559d85f452c406faf2089ac
Oluwatobi <amure387@gmail.com>
2023-06-29 22:29:38 +0000

    fix: Handled error for 100-await

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,9 +1,4 @@
-import guardrail from './9-try';
-import divideFunction from './8-try';
-
-console.log(guardrail(() => { return divideFunction(10, 2)}));
-console.log(guardrail(() => { return divideFunction(10, 0)}));
-/* import asyncUploadUser from "./100-await";
+import asyncUploadUser from "./100-await";
 
 const test = async () => {
     const value = await asyncUploadUser();
@@ -11,4 +6,3 @@ const test = async () => {
 };
 
 test();
-*/
