ad76ba5136c7f18d8d7380ae7c0f1c0cfaefa9f5
Oluwatobi <amure387@gmail.com>
2023-07-04 19:59:48 +0000

    fix: eslint no-param-reassign 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
   return getListStudents.filter((ele) => ele.location === city).map((ele) => {
     const gradeObj = newGrades.find((grade) => grade.studentId === ele.id);
-    ele.grade = gradeObj ? gradeObj.grade : 'N/A'; // eslint-disable no-param-reassign
+    ele.grade = gradeObj ? gradeObj.grade : 'N/A'; // eslint-disable-line no-param-reassign
     return ele;
   });
 }
