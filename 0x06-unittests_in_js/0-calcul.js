ce7b22f8a4d302099ddd321a87a1f28d0b5110d2
Oluwatobi <amure387@gmail.com>
2023-09-20 20:46:18 +0000

    refactored task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,7 @@
-#!/usr/bin/node
 function calculateNumber(a, b) {
  const sum = Math.round(a) + Math.round(b) 
  console.log(sum)
  return sum
 }
-
-calculateNumber(-3.9, 4.5)
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,10 +1,9 @@
 const getPaymentTokenFromAPI = require('./6-payment_token')
-const sinon =require('sinon')
-describe('getPaymentTokenFromAPI', (done) => {
-    it('should return success message if success is true', () => {
+describe('getPaymentTokenFromAPI', () => {
+    it('should return success message if success is true', (done) => {
         getPaymentTokenFromAPI(true).then((data) => {
         expect(data).to.equal({data: 'Successful response from the API' })
-        done()
     })
+        done()
     })
 })
