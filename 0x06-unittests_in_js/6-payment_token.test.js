4abfceb5704941061eda4f93a0cf7a07bd8aff03
Oluwatobi <amure387@gmail.com>
2023-09-21 07:05:17 +0000

    fix: imported expect from chai

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ describe('getPaymentTokenFromAPI', () => {
     it('should return success message if success is true', (done) => {
         getPaymentTokenFromAPI(true).then((data) => {
         expect(data).to.equal({data: 'Successful response from the API' })
-    })
         done()
+    })
     })
 })
