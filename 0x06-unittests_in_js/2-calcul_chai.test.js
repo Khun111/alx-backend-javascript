a5cd904c4364eb859f1107fe314f873dd37531c8
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-09-21 07:55:34 +0100

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-const calculateNumber = require("./2-calcul.js");
+const calculateNumber = require("./2-calcul_chai");
 const { expect } = require('chai')
 const { describe, it } = require('mocha')
 
