154750fc048206420afdbb6fe8bcaf9550ad6a05
Oluwatobi <amure387@gmail.com>
2023-09-21 07:02:04 +0000

    fix: imported expect from chai

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,4 +1,6 @@
 const getPaymentTokenFromAPI = require('./6-payment_token')
+const { expect } = require('chai')
+
 describe('getPaymentTokenFromAPI', () => {
     it('should return success message if success is true', (done) => {
         getPaymentTokenFromAPI(true).then((data) => {
