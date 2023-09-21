c6948d2418fb67341e0a45d9637c6bb8fc4ce159
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-09-21 22:22:38 +0100

    Update api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,7 +1,5 @@
 const request = require('request')
 const { expect } = require('chai')
-// eslint-disable-next-line no-unused-vars
-const app = require('./api')
 
 describe('Test index Page', () => {
     it('should return the correct status code', (done) => {
